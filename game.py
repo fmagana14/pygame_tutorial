e332c85f495c09bcb45e35a080fbc825f3b88132
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-12-06 20:29:22 -0800

    finished surfaces

--- game.py
+++ game.py
@@ -18,31 +18,17 @@ while running:
 
     # # Clear the Screen
 
-    # color: blue
+   # Configure the screen
+
+    # Create a new instance of surface
+    surf = pygame.Surface([50,50])
+    surf.fill((255, 111, 33))
+
+    # clear screen
     screen.fill((255, 255, 255))
-    color = (0,191,255)
-    position = (400,400)
-    pygame.draw.circle(screen, color, position, 75)
 
-    # color: Red
-    color = (255, 0, 0)
-    position = (80, 80)
-    pygame.draw.circle(screen, color, position, 75)
-
-    # color: Yellow
-    color = (255, 255, 0)
-    position = (250, 250)
-    pygame.draw.circle(screen, color, position, 75)
-   
-    # color: Green
-    color = (0,255,0)
-    position = (80,400)
-    pygame.draw.circle(screen, color, position, 75)
-
-    # color: Orange
-    color = (255,128,0)
-    position = (400,80)
-    pygame.draw.circle(screen, color, position, 75)  
+    # Draw the surface 
+    screen.blit(surf, (100, 120))
     
     pygame.display.flip()
 
