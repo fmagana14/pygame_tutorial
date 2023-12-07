6bea0bd0a31903f9ae0a84fe6da7778502768b91
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-12-06 21:26:20 -0800

    completed part 2

--- game.py
+++ game.py
@@ -5,31 +5,73 @@ pygame.init()
 # creating the screen size
 screen = pygame.display.set_mode([500,500])
 
-# rest of your code...
 
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super(GameObject, self).__init__()
+        self.surf = pygame.image.load(image)
+        self.rect = self.surf.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-# making the game loop
+    def render(self, screen):
+        screen.blit(self.surf, self.rect)
+
+# Create an instance of GameObject
+apple = GameObject(120, 300, 'apple.png')
+
+# Making the game loop
 running = True
-while running: 
-    # looks at event
+clock = pygame.time.Clock()
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # # Clear the Screen
-
-   # Configure the screen
-
-    # Create a new instance of surface
-    surf = pygame.Surface([50,50])
-    surf.fill((255, 111, 33))
-
-    # clear screen
+    # Clear the screen
     screen.fill((255, 255, 255))
 
-    # Draw the surface 
-    screen.blit(surf, (100, 120))
-    
+    # Draw the game objects
+    apple.render(screen)
+
+    # Update the window
     pygame.display.flip()
 
-# WHEN TYPING THE FOLLOWING: 
+    # Cap the frame rate
+    clock.tick(60)
+
+# Quit the pygame
+pygame.quit()
+
+
+
+# class GameObject(pygame.sprite.Sprite):
+#   def __init__(self, x, y, width, height):
+#     super(GameObject, self).__init__()
+#     self.surf = pygame.Surface((width, height))
+#     self.surf.fill((255, 0, 255))
+#     self.rect = self.surf.get_rect()
+#     self.x = x
+#     self.y = y
+
+#   def render(self, screen):
+#     screen.blit(self.surf, (self.x, self.y))
+
+# box = GameObject(120, 300, 50, 50)
+
+# # making the game loop
+# running = True
+# while running: 
+#     # looks at event
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+# # Clear screen
+# screen.fill((255, 255, 255))
+# # Draw box
+# box.render(screen)
+# # Update the window
+# pygame.display.flip()
+# # WHEN TYPING THE FOLLOWING: 
